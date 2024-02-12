2b61df57f0b942b7ab7f4207a1799619dca4db14
AlexD87 <alexander.dause@web.de>
2024-02-12 20:24:57 +0100

    update animations

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,22 +1,45 @@
 import { Component, OnInit } from '@angular/core';
-import 'animate.css';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
-  styleUrl: './main-page.component.scss'
+  styleUrls: ['./main-page.component.scss'],
+  animations: [
+    trigger('titleOneAnimation', [
+      state('inactive', style({
+        transform: 'translateX(1200px)' // Start position
+      })),
+      state('active', style({
+        transform: 'translateX(0)' // End position
+      })),
+      transition('inactive => active', animate('1s ease-in-out')),
+      transition('active => inactive', animate('1s ease-in-out'))
+    ]),
+    trigger('titleTwoAnimation', [
+      state('inactive', style({
+        transform: 'translateX(-1200px)' // Start position
+      })),
+      state('active', style({
+        transform: 'translateX(0)' // End position
+      })),
+      transition('inactive => active', animate('1s ease-in-out')),
+      transition('active => inactive', animate('1s ease-in-out'))
+    ])
+  ]
 })
 export class MainPageComponent implements OnInit {
+  titleOneState: string = 'inactive';
+  titleTwoState: string = 'inactive';
+
   ngOnInit(): void {
-    mainPageSlideIn()
+    setTimeout(() => {
+      this.titleOneState = 'active';
+    }, 1500);
+    setTimeout(() => {
+      this.titleTwoState = 'active';
+    }, 1500);
   }
-
 }
-function mainPageSlideIn() {
-  document.addEventListener("DOMContentLoaded", () => {
-    setTimeout( () => {
 
-    }, 800)
-  } )
-}
 
