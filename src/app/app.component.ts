ad625678a1e1ceda6cdd08a50a507803622776b5
AlexD87 <alexander.dause@web.de>
2024-02-16 10:34:02 +0100

    update menu function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+handleMenuChange($event: boolean) {
+throw new Error('Method not implemented.');
+}
   title = 'myPortfolio';
 }
